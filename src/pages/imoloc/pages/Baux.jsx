4c5d8a02c5d1631a050f5550a445c03ebbba1a1a
djlo1 <djloalo@gmail.com>
2026-04-19 06:13:28 +0100

    fix: useRef manquant dans import

--- src/pages/imoloc/pages/Baux.jsx
+++ src/pages/imoloc/pages/Baux.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../../../lib/supabase'
 import toast from 'react-hot-toast'
